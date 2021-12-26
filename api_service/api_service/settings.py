1d68a7c12de16df037efc6fbf95b489b6f584c3c
Your Name <you@example.com>
2021-12-26 22:18:12 +0000

    feat: add JWT lib

--- api_service/api_service/settings.py
+++ api_service/api_service/settings.py
@@ -117,4 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
--- api_service/api_service/urls.py
+++ api_service/api_service/urls.py
@@ -2,6 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from api import views as api_views
 
@@ -10,4 +14,6 @@ urlpatterns = [
     path('history', api_views.HistoryView.as_view()),
     path('stats', api_views.StatsView.as_view()),
     path('admin', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
