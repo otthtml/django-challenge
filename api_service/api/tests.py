07accf70865be661b426be2d430ea5aa0bd2a4c1
Your Name <you@example.com>
2021-12-26 22:19:02 +0000

    chore: configure api_service tests

--- api_service/api/tests.py
+++ api_service/api/tests.py
@@ -1,3 +1,12 @@
+'''
+Test for api_service
+'''
 from django.test import TestCase
 
 # Create your tests here.
+class TestJWT(TestCase):
+    '''Tests for JWT auth'''
+
+    def test_jwt_create(self):
+        '''Test JWT creation'''
+        self.assertEqual(True, 1==1)
