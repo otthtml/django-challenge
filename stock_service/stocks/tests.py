48d62ffd4c2d4b949810fef91ca059e313c585bb
Your Name <you@example.com>
2021-12-26 22:18:40 +0000

    chore: configure stock_service tests

--- stock_service/stocks/tests.py
+++ stock_service/stocks/tests.py
@@ -1,3 +1,12 @@
+'''
+Test for stock_service
+'''
 from django.test import TestCase
 
 # Create your tests here.
+class TestStock(TestCase):
+    '''Tests for external calls'''
+
+    def test_stooq_returns_csv(self):
+        '''Test calls to stooq are returning as expected'''
+        self.assertEqual(True, 1==1)
