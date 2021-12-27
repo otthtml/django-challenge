6220ae60dceaadecaeec731523ca8bc9fadf511e
Your Name <you@example.com>
2021-12-27 03:00:38 +0000

    feat: add /stock endpoint to stock_service

--- stock_service/stocks/views.py
+++ stock_service/stocks/views.py
@@ -2,7 +2,7 @@
 '''View that contains all of stock_service's endpoints'''
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+import requests
 
 class StockView(APIView):
     '''
@@ -12,5 +12,6 @@ class StockView(APIView):
         '''Takes a stock code, builds a request, sends it to external
         and returns result to requester'''
         stock_code = request.query_params.get('stock_code')
-        
-        return Response()
+        query = f'https://stooq.com/q/l/?s={stock_code}&f=sd2t2ohlcvn&h&e=csv​'
+        response = requests.request("GET", query)
+        return Response(response.text)
