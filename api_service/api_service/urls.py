ce4d91f7f9fcfd6d66107ce828e07daedbd876c2
Your Name <you@example.com>
2021-12-27 21:12:41 +0000

    chore: clean urls up

--- api_service/api_service/urls.py
+++ api_service/api_service/urls.py
@@ -2,10 +2,6 @@
 
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from api import views as api_views
 
@@ -14,6 +10,4 @@ urlpatterns = [
     path('history', api_views.HistoryView.as_view()),
     path('stats', api_views.StatsView.as_view()),
     path('admin', admin.site.urls),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
