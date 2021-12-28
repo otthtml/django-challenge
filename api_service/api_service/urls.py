98cef0d74a9557debe379d01c4e3f387e60b0fe1
Your Name <you@example.com>
2021-12-28 00:24:11 +0000

    feat: add token urls for Postman testing

--- api_service/api_service/urls.py
+++ api_service/api_service/urls.py
@@ -3,6 +3,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from api import views as api_views
 
@@ -11,4 +15,6 @@ urlpatterns = [
     path('history', api_views.HistoryView.as_view()),
     path('stats', api_views.StatsView.as_view()),
     path('admin', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
