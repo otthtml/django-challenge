ed563e1dc1bd4f4be216b620cd64f2b212c065ae
Your Name <you@example.com>
2021-12-31 23:14:36 +0000

    feat: clean, format and save stock_data

--- api_service/api/models.py
+++ api_service/api/models.py
@@ -2,13 +2,13 @@
 
 from django.conf import settings
 from django.db import models
-
+from api import constants
 
 class UserRequestHistory(models.Model):
     """
     Model to store the requests done by each user.
     """
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=20)
     open = models.DecimalField(max_digits=10, decimal_places=2)
@@ -16,3 +16,15 @@ class UserRequestHistory(models.Model):
     low = models.DecimalField(max_digits=10, decimal_places=2)
     close = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    @classmethod
+    def insert_stock_data(cls, user, *stock_data):
+        return cls(
+            user=user,
+            name=stock_data[constants.NAME],
+            symbol=stock_data[constants.SYMBOL],
+            open=stock_data[constants.OPEN],
+            high=stock_data[constants.HIGH],
+            low=stock_data[constants.LOW],
+            close=stock_data[constants.CLOSE],
+        )
--- api_service/api/views.py
+++ api_service/api/views.py
@@ -1,6 +1,5 @@
 # encoding: utf-8
 '''View that contains all of api_service's endpoints'''
-
 import requests
 
 from rest_framework import generics
@@ -10,13 +9,31 @@ from rest_framework.response import Response
 
 from api.models import UserRequestHistory
 from api.serializers import UserRequestHistorySerializer
+from api import constants
 
-STOCK_SERVICE_URL = 'http://localhost:9000'
+def _clean_stock_data(stock_data: str):
+    clean_data = stock_data.split(',')
+    clean_data[constants.SYMBOL] = clean_data[constants.SYMBOL].split('\\n')[1]
+    clean_data[constants.NAME] = clean_data[constants.NAME].split('\\')[0]
+    return clean_data
 
-def save_stock_data(stock_data):
-    '''save stock data fetched from stock_service to DB'''
-    print(stock_data)
+def _format_stock_data(stock_data):
+    formated = {
+        'name': stock_data[constants.NAME],
+        'symbol': stock_data[constants.SYMBOL],
+        'open': _to_int_or_float(stock_data[constants.OPEN]),
+        'high': _to_int_or_float(stock_data[constants.HIGH]),
+        'low': _to_int_or_float(stock_data[constants.LOW]),
+        'close': _to_int_or_float(stock_data[constants.CLOSE])
+    }
+    return formated
 
+def _to_int_or_float(number):
+    try:
+        number = int(number)
+    except ValueError:
+        number = float(number)
+    return number
 
 class StockView(APIView):
     """
@@ -27,9 +44,18 @@ class StockView(APIView):
     def get(self, request):
         '''Call the stock service, save the response, parse it and return it to the user'''
         stock_code = request.query_params.get('stock_code')
-        query = f'{STOCK_SERVICE_URL}/stock?stock_code={stock_code}'
-        stock_data = requests.get(query)
-        return Response(stock_data.text)
+        query = f'{constants.STOCK_SERVICE_URL}/stock?stock_code={stock_code}'
+        stock_data = requests.get(query).text
+        stock_data = _clean_stock_data(stock_data)
+        for i in UserRequestHistory.objects.all():
+            print(i.date)
+            print(i.name)
+        UserRequestHistory.insert_stock_data(request.user, *stock_data)
+        for i in UserRequestHistory.objects.all():
+            print(i.date)
+            print(i.name)
+
+        return Response(_format_stock_data(stock_data))
 
 
 class HistoryView(generics.ListAPIView):
