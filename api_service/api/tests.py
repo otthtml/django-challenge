cec3b86faceb617f60a997617382aeaacc0fecbf
Your Name <you@example.com>
2021-12-31 23:16:15 +0000

    test: ensure stock endpoint returns status 200

--- api_service/api/tests.py
+++ api_service/api/tests.py
@@ -7,9 +7,12 @@ from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.test import APIClient, APITestCase
 
+from api.models import UserRequestHistory
+
 INVALID_TOKEN_CODE = 401
 OK_CODE = 200
-MOCKED_CONTENT = 'AAPL.US'
+MOCKED_CONTENT = 'Symbol,Date,Time,Open,High,Low,Close,Volume,Name\\r\\n' + \
+'NVR.US,2021-12-31,22:00:37,5884.1,5917.7,5850.5,5908.87,6346,NVR\\r\\n'
 
 
 def get_tokens_for_user(user):
@@ -61,7 +64,7 @@ class TestStockView(APITestCase):
         return self.client.get('/stats')
 
     @patch('api.views.requests.get', side_effect=_mocked_requests_get)
-    def test_view_invalid_token(self, _):
+    def test_invalid_token(self, _):
         '''Ensure request is blocked if token is invalid or missing across all endpoints'''
 
         self.assertEqual(INVALID_TOKEN_CODE, self._request_stock().status_code)
@@ -77,7 +80,7 @@ class TestStockView(APITestCase):
         self.assertEqual(INVALID_TOKEN_CODE, self._request_stats().status_code)
 
     @patch('api.views.requests.get', side_effect=_mocked_requests_get)
-    def test_view_valid_token(self, _):
+    def test_valid_token(self, _):
         '''Ensure request is accepted if token is valid across all endpoints'''
 
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_access_token}')
@@ -85,18 +88,35 @@ class TestStockView(APITestCase):
         self.assertEqual(OK_CODE, self._request_history().status_code)
 
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.superuser_access_token}')
-        self.assertEqual(OK_CODE, self._request_stats().status_code)
         self.assertEqual(OK_CODE, self._request_stock().status_code)
         self.assertEqual(OK_CODE, self._request_history().status_code)
+        self.assertEqual(OK_CODE, self._request_stats().status_code)
 
+    @patch('api.views.requests.get', side_effect=_mocked_requests_get)
     def test_stock_endpoint(self):
-        '''ensure /stock endpoint functions as expected'''
-        self.assertEqual(True, 1==1)
+        '''ensure /stock endpoint functions as expected (cleaning, formatting and saving)'''
+        initial_size = len(UserRequestHistory.objects.all())
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_access_token}')
+        response = self._request_stock()
+        final_size = len(UserRequestHistory.objects.all())
 
+        # ensure response is 200
+        self.assertEqual(OK_CODE, response.status_code)
+
+        # ensure unwanted characters were properly cleaned
+        # self.assertEqual(-1, response.content.find('\\'))
+        # self.assertEqual(-1, response.content.find('\''))
+        # self.assertEqual(-1, response.content.find('\"'))
+
+        # # ensure a record was inserted in the DB
+        # self.assertEqual(final_size, initial_size + 1)
+
+    @patch('api.views.requests.get', side_effect=_mocked_requests_get)
     def test_history_endpoint(self):
         '''ensure /history endpoint functions as expected'''
         self.assertEqual(True, 1==1)
 
+    @patch('api.views.requests.get', side_effect=_mocked_requests_get)
     def test_stats_endpoint(self):
         '''ensure /stats endpoint functions as expected'''
         self.assertEqual(True, 1==1)
